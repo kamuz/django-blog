17fc91085749ce1cadadf8da27de120733ef297b
Vladimir Kamuz <v.kamuz@gmail.com>
2024-11-28 22:19:38 +0200

    Edit admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status', 'sticked_post']
+    list_display_links = ['author']
+    list_filter = ['status', 'created', 'publish', 'author']
+    list_editable = ['sticked_post', 'status']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    show_facets = admin.ShowFacets.ALWAYS
