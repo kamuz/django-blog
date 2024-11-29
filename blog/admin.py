f9534f73cd4fa6eaba31acaff9c5e10a618842ab
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-11-29 09:00:54 +0200

    Edit post admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,6 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'publish', 'status', 'sticked_post']
-    list_display_links = ['author']
     list_filter = ['status', 'created', 'publish', 'author']
     list_editable = ['sticked_post', 'status']
     search_fields = ['title', 'body']
