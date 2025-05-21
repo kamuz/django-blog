fc44cdfd2768f7e4a92bc97d56e5158046cde054
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-05-21 12:46:45 +0300

    Recommending posts by email

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,4 @@
+from decouple import config
 from pathlib import Path
 import os
 
@@ -108,3 +109,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
